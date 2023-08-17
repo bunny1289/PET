5a8e1bff477349449a646475299142d833201b62
bunny1289 <abhilash.d20@iiits.in>
2023-08-17 13:50:24 +0530

    screenshot

---
+++ screenshot.py
@@ -0,0 +1,30 @@
+import keyboard
+import time
+from PIL import ImageGrab
+
+def take_screenshot():
+    # Get the current time for naming the screenshot
+    timestamp = time.strftime("%Y-%m-%d_%H-%M-%S")
+    
+    # Capture the screenshot
+    screenshot = ImageGrab.grab()
+    
+    # Save the screenshot
+    screenshot.save(f"screenshot_{timestamp}.png")
+    print(f"Screenshot saved as screenshot_{timestamp}.png")
+
+def main():
+    print("Press a + S to take a screenshot.")
+    
+    # Listen for the Ctrl + Shift + S combination
+    keyboard.add_hotkey("a+s", take_screenshot)
+    
+    try:
+        keyboard.wait("esc")  # Wait for the 'esc' key to exit the program
+    except KeyboardInterrupt:
+        pass
+    finally:
+        keyboard.remove_hotkey("a+s")  # Remove the hotkey listener
+
+if __name__ == "__main__":
+    main()
